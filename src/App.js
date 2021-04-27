d7748b059ff9b5bef57ebae68f8ccda2c0a3545b
Natnael Eshetu <natnaels@hotmail.com>
2021-04-27 12:54:55 -0700

    Done with python eda & initial formatting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import React from "react";
+
+import { useFetch } from "../hooks/useFetch";
+import 'regenerator-runtime/runtime'; //needed NO IDEA WHY BUT FUCK IT LMFAOOOO
+import * as d3 from "d3";
+
 const viewHeight = 500;
 const viewWidth = 500;
 const App = () => {
+    const [data, loading] = useFetch(
+        "https://raw.githubusercontent.com/NatnaelE/dataViz/master/a1_data.csv"
+      );
+     //console.log("data", loading, data);
+      const size = 500;
+      const margin = 20;
+      const axisTextAlignmentFactor = 3;
+
     return (
-    <svg style={{ border: "1px solid black", width: viewWidth, height: viewHeight}}>
-        <circle cx={200} cy={20} r="5"></circle>
-        <rect x ={20} y = {50} width = "10" height="10" fill="rgb(230,230,230)"></rect>
-        <rect x ={40} y = {50} width = "10" height="10" fill="rgb(230,230,230)"></rect>
-        <rect x ={60} y = {50} width = "10" height="10" fill="rgb(230,230,230)"></rect>
-        <rect x ={80} y = {50} width = "10" height="10" fill="rgb(230,230,230)"></rect>
-        <rect x ={100} y = {50} width = "10" height="10"></rect>
-        <line x1="0" y1={viewHeight} x2={150} y2={200} stroke="black"></line>
-        <line x1="20" y1={viewHeight - 20} x2={150} y2={200} stroke="black"></line>
-        <line x1="0" y1={viewHeight - 50} x2={150} y2={200} stroke="black"></line>
-        <text x="20" y="35" class="small">
-            "FUCK FUCK FUCK FUCK FUCK FUCK "
-        </text>
-    </svg>
+    <div>
+        <h1>Assignment 2: Exploratory Data Analysis</h1>
+        <h3>Data Background:
+            <p>place holder text</p>
+        </h3>
+        <h4>EDA</h4>
+        <h3>
+            Initial Questions:
+            <li>What has been the most common crime so far in the last year?</li>
+            <li>What type of location do most crimes occur in?</li>
+            <li>Which areas have the most Homicides/Murders?</li>
+        </h3>
+        <h3>Visual Analysis</h3>
+        <h6>Viz 1</h6>
+        <svg width={size} height={size} style={{ border: "1px solid black" }}>
+            
+        </svg>
+        <h6>Viz 2</h6>
+        <h6>Viz 3</h6>
+        <h6>Viz 4</h6>
+        <h6>Viz 5</h6>
+        <h6>Viz 6</h6>
+        <h6>Viz 7</h6>
+        <h6>Viz 8</h6>
+        <h3>Closing Statements</h3>
+    </div>
     );
 }
 
